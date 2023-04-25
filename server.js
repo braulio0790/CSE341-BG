1f0e65d6de8e687aad3ca24b24d915741abf2b77
braulio0790 <braulio90.lds@gmail.com>
2023-04-24 21:11:07 -0600

    more progress

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 //express web server
 const express = require('express');
 const app = express();
+const lesson1Controller = require('../controllers/lesson1');
 
-app.get('/', (req,res) => {
-  res.send("Braulio Garcia");
-});
+app.get('/', lesson1Controller.marianaGarcia);
+app.get('/mari', lesson1Controller.mariaOlvera);
 
 app.listen(process.env.port || 3000);
 console.log('Web Server is listening at port '+ (process.env.port || 3000));

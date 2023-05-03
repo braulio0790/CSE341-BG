352957d580ef6b20a0aed87442cf9b14f28d02c0
braulio0790 <braulio80.lds@gmail.com>
2023-05-02 18:46:36 -0600

    New Commit

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-
-const routes = require('express').Router();
-const lesson1Controller = require('../controllers/lesson1');
-
-routes.get('/', lesson1Controller.marianaGarcia);
-routes.get('/mariaOlvera', lesson1Controller.mariaOlvera);
-
-module.exports = routes;

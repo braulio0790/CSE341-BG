3be40d76c6c6a5114fd8bbc2602012b795866b4b
braulio0790 <braulio80.lds@gmail.com>
2023-05-02 20:50:19 -0600

    More

---
+++ app.js
@@ -0,0 +1,11 @@
+//express web server
+const express = require('express');
+const app = express();
+//const lesson1Controller = require('./controllers/lesson1');
+const connectDB = require('./mongoDB/conectionDB');
+
+connectDB();
+const port = 3000;
+
+app.listen(process.env.port || port);
+console.log('Web Server is listening at port ' + (process.env.port || port));
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 //import dot enviroment packages
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 //import mongo db package
-const mongodb = require('mongodb');
+const mongodb = require("mongodb").mongodb;
 
 
 //which data base we want to connect and username/password in the other parameter
 //The code below is how we can actually work with the db
-mongodb.connect(process.env.CONNECTIONSTRING, async function(err,client){
+mongodb.connect(process.env.CONNECTIONSTRING, { useUnifiedTopology: true}, async function(err,client){
    //this is going to call the especific db or collection
    const db = client.db();
    //Wit this we can call all the data, or using the find method call something especific
-   const results = await db.collection('pets').find().toArray();
+   const results = await db.collection("pets").find().toArray();
    console.log(results);
    client.close();
 });
